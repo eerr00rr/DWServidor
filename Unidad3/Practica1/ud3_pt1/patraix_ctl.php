70f0a67c47c592ecc57467ae3710da5bd1eea326
eerr00rr <alex.11.monti@gmail.com>
2024-09-24 13:36:04 +0200

    done

--- Unidad3/Practica1/ud3_pt1/patraix_ctl.php
+++ Unidad3/Practica1/ud3_pt1/patraix_ctl.php
@@ -6,22 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        $select = $_POST['datos_patraix'];
+    <?php
+        include 'datos_patraix.php'; 
+
+        $select = $_POST['barrios'];
+        $valor = $datos_patraix[$select];
 
         $nombre = $_POST["nombre"];
         $apellidos = $_POST["apellidos"];
+
         echo "<h1>Bienvenido $nombre $apellidos !</h1>";
-        if (isset($select)) {
+        if ($select === 'todos') {
             echo "<h1>Mostrando los datos demográficos de todos los barrios del distrito Patraix</h1>";
             echo "<ul>";
-            foreach ($select as $distrito => $habitantes) {
-                echo "<li>$distrito[0]: $distrito[1] hab.</li>";
+            foreach ($datos_patraix as $distrito => $valor) {
+                echo "<li>$valor[0]: $valor[1] hab.</li>";
             }
             echo "</ul>";
         } else {
-            $valor = $select[1];
-            echo "El distrito $select[0] tiene $valor<br>";
+            echo "El distrito $valor[0] tiene $valor[1]<br>";
         }
         echo "<a href='menu.php?nombre=$nombre&apellidos=$apellidos'>Inicio</a>";
     ?>
