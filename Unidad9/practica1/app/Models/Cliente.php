8543dd5a6ba5d50e75b0db1aab29571a1a311750
eerr00rr <alex.11.monti@gmail.com>
2024-12-12 19:24:01 +0100

    unfinished

--- Unidad9/practica1/app/Models/Cliente.php
+++ Unidad9/practica1/app/Models/Cliente.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Cliente extends Model
 {
     use HasFactory;
@@ -12,5 +13,17 @@ class Cliente extends Model
     public function getNombreApellidos(): string
     {
         return "$this->nombre $this->apellidos";
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'fechaN' => 'datetime:d-m-Y',
+        ];
     }
 }
